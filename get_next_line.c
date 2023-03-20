05364e3ed29e1450bc33f5f4e430bc11ba566f10
Irenegs <gonzalezsaavedrairene@gmail.com>
2023-03-20 17:20:44 +0100

    20-03

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: irgonzal <irgonzal@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: irgonzal <irgonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 17:48:33 by irgonzal          #+#    #+#             */
-/*   Updated: 2023/01/23 17:48:36 by irgonzal         ###   ########.fr       */
+/*   Updated: 2023/01/24 13:38:22 by irgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@ static size_t	contains_line(char *s)
 	i = 0;
 	while (s[i] != '\0' && s[i] != '\n')
 		i++;
-	if (s[i] == '\n')
-		return (i + 1);
-	return (0);
+	return (i);
 }
 
 char	*prepare_line(char *aux, size_t pos_nl)
 {
 	char	*line;
 
-	line = ft_substr(aux, 0, pos_nl);
+	line = NULL;
+	if (ft_strlen(aux) != 0)
+		line = ft_substr(aux, 0, pos_nl);
 	free(aux);
 	return (line);
 }
@@ -43,45 +43,43 @@ static char	*load_buffer(char *rem, int fd)
 	if (!buffer)
 		return (NULL);
 	bytes = read(fd, buffer, BUFFER_SIZE);
-	if (bytes == 0)
-		return ("\0");//demasiado drástico?
-	buffer[BUFFER_SIZE] = '\0';
-	//printf("buffer %s|\n", buffer);
+	buffer[bytes] = '\0';
+	aux = ft_strdup(rem);
 	if (rem[0] != '\0')
-	{
-		aux = ft_strdup(rem);
 		free(rem);
-	}
-	else
-		aux = "\0";
 	rem = ft_strjoin(aux, buffer);
-	if (aux[0] != '\0')
-		free(aux);
+	free(aux);
 	free(buffer);
 	return (rem);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*remains = "\0";
+	static char	*rem = "\0";
 	char		*aux;
 	size_t		pos_nl;
 
-	if (remains[0] == '\0')
-		remains = load_buffer(remains, fd);
-	if (!remains || remains[0] == '\0')
+	if (fd < 1)
 		return (NULL);
-	while (contains_line(remains) == 0 && ft_strlen(remains) != 0)
+	if (rem[0] == '\0')
+		rem = load_buffer(rem, fd);
+	if (!rem || rem[0] == '\0')
+		return (NULL);
+	pos_nl = 0;
+	printf("%lu %lu", pos_nl, contains_line(rem));
+	while (ft_strlen(rem) == contains_line(rem) && pos_nl != contains_line(rem))
 	{
-		remains = load_buffer(remains, fd);
-		if (!remains || remains[0] == '\0')
+		pos_nl = contains_line(rem);
+		rem = load_buffer(rem, fd);
+		if (!rem || rem[0] == '\0')
 			return (NULL);
+		printf("%lu %lu", pos_nl, contains_line(rem));
 	}
-	aux = ft_strdup(remains);
+	aux = ft_strdup(rem);
 	if (!aux)
 		return (NULL);
 	pos_nl = contains_line(aux);
-	free(remains);
-	remains = ft_substr(aux, pos_nl, ft_strlen(aux) - pos_nl);
+	free(rem);
+	rem = ft_substr(aux, pos_nl, ft_strlen(aux) - pos_nl);
 	return (prepare_line(aux, pos_nl));
 }
